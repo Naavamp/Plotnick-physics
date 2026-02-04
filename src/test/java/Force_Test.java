c49a206e91d6e813c861f869ff16c7ac71f92d9c
Naava  Plotnick <naavamp@gmail.com>
2026-02-04 18:27:48 -0500

    Added Force and ForceTest Class

---
+++ src/test/java/Force_Test.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForceTest
+{
+
+    @org.junit.jupiter.api.Test
+    void getDegrees()
+    {
+        //given
+        Force f = new Force(3, 4);
+
+        //when
+        double degrees = f.getDegrees();
+
+        //then
+        assertEquals(53.13, degrees, 0.1);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMagnitude()
+    {
+
+        //given
+        Force f = new Force(3, 4);
+
+        //when
+        double magnitude = f.getMagnitude();
+
+        //then
+        assertEquals(5, magnitude, 0.1);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void add()
+    {
+
+        //given
+        Force f1 = new Force(3, 4);
+        Force f2 = new Force(4, 4);
+
+        //when
+        Force f3 = f1.add(f2);
+
+        //then
+        assertEquals(7, f3.getX());
+        assertEquals(8, f3.getY());
+    }
+
+    @Test
+    void normalize()
+    {
+        //given
+        Force f1 = new Force(3, 4);
+
+        //when
+        Force normalized = f1.normalize();
+
+        //then
+        assertEquals(1,normalized.getMagnitude());
+    }
+
+    @Test
+    void scale()
+    {
+        //given
+        Force f1 = new Force(3, 4);
+
+        //when
+        Force scaled = f1.scale(2);
+
+        //then
+        assertEquals(6, scaled.getX());
+        assertEquals(8, scaled.getY());
+    }
+}
